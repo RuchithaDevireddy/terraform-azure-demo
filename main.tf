3f5c7573626c42f08885b163c23d8394064a4d98
shiva <narrashivakumarreddy@gmail.com>
2025-10-31 14:04:25 +0000

    Initial Terraform setup

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  required_version = ">= 1.5.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Create a Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "rg-terraform-demo"
+  location = "East US"
+}
+
+# Create a Storage Account
+resource "azurerm_storage_account" "example" {
+  name                     = "tfstoragedemo1234"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}

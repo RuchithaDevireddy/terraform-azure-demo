a1a3720684ee4086f44280537ce43734c1be4a69
shiva <narrashivakumarreddy@gmail.com>
2025-10-31 16:41:22 +0000

    changing storage account name

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "azurerm_resource_group" "example" {
 
 # Create a Storage Account
 resource "azurerm_storage_account" "example" {
-  name                     = "tfstoragedemotgrfgh"
+  name                     = "tfstoragedemotgrfgh1"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
